150917239d3b1c4069aff830d8290d3d08b37309
digitz20 <alaekekaebuka200@gmail.com>
2026-08-06 22:41:40 +0100

    first commit

---
+++ launch-systemstartup.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Launching System Startup 2.0...
+echo.
+echo If you see an antivirus warning, this is because this is a Windows installer file that makes system changes.
+echo If you trust this file, you can allow it to run.
+echo.
+start "" "systemstartup2.0.msi"
+pause
